9126aa2920b309fa26fa65f0c6f7533f8efea19e
1970Mr <mrepoch1970@gmail.com>
2024-08-05 15:47:16 +0430

    Creating the basic structure

---
+++ src/RedirectorServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Mr1970\LaravelRedirector;
+
+use Illuminate\Support\ServiceProvider;
+
+class RedirectorServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
