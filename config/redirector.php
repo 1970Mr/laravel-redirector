9fda60db59acfb159b9f5b8aa168ee98525b71ad
1970Mr <mrepoch1970@gmail.com>
2024-08-05 17:07:57 +0430

    Add config, and update service provider

---
+++ config/redirector.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'cache_method' => env('REDIRECTOR_CACHE_METHOD', 'multiple'), // Options: 'multiple', 'single'
+];
--- src/RedirectorServiceProvider.php
+++ src/RedirectorServiceProvider.php
@@ -13,6 +13,20 @@ class RedirectorServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        //
+        $this->loadMigrationsFrom($this->baseBath('database/migrations'));
+        $this->mergeConfigFrom($this->baseBath('database/config'), 'redirector');
+
+        $this->publishes([
+            $this->baseBath('database/migrations') => 'database/migrations'
+        ], 'laravel-redirector-migrations');
+
+        $this->publishes([
+            $this->baseBath('config') => 'config'
+        ], 'laravel-redirector-config');
+    }
+
+    private function baseBath(string $path): string
+    {
+        return __DIR__ . '/../' . trim($path, '/');
     }
 }
